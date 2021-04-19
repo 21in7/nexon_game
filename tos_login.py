07a7cad5cab5211ad53d3e847c5047501b7d25d5
21in7 <skawksmsduzo@gmail.com>
2021-04-19 11:39:50 +0900

    update_code

--- tos_login.py
+++ tos_login.py
@@ -1,31 +1,53 @@
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import TimeoutException
+import sys
 
-# 익스플로러 드라이버 선택
-driver = webdriver.Ie('')
+# 인터넷 익스플로러 드라이버 위치
+driver = webdriver.Ie('E:\python\IEDriverServer.exe')
+print("프로그램 실행중....")
 
-driver.implicitly_wait(3)
 driver.set_window_size(1280, 720)
 driver.get('http://tos.nexon.com/')
 
-nx_login = driver.find_element_by_class_name("login_btn")
-nx_login.click()
 
-nx_id = driver.find_element_by_id('txtNexonID')
-nx_pw = driver.find_element_by_id('txtPWD')
-nx_submit = driver.find_element_by_id('btnLogin')
+try:    # 트오세 이벤트 페이지로 시작할시
+    nx_login = driver.find_element_by_class_name("gnbLogin")
+    nx_login.click()
+except NoSuchElementException:  # 트오세 메인 홈페이지로 시작할시
+    btn_nx = driver.find_element_by_class_name("login_btn")
+    btn_nx.click()
+finally:
+    nx_id = driver.find_element_by_id('txtNexonID')
+    nx_pw = driver.find_element_by_id('txtPWD')
+    nx_sumbit = driver.find_element_by_id('btnLogin')
 
 while driver.get:
     nx_id.clear()
-    # 넥슨아이디
+    # 넥슨 아이디
     nx_id.send_keys("")
-    
+
     nx_pw.clear()
     # 넥슨 비밀번호
     nx_pw.send_keys("")
-    
-    nx_submit.click()
+
+    nx_sumbit.click()
     break
-gamestart = driver.find_element_by_id('tmGameStart')
-gamestart.click()
-driver.close()
-driver.quit()
+
+
+try:    # 던파 이벤트 페이지로 바로 시작하면 실행
+    gamestart = driver.find_element_by_class_name('gameStart')
+    gamestart.click()
+    driver.close()
+    print("프로그램 실행 완료")
+    print("프로그램을 종료합니다...")
+    driver.quit()
+except NoSuchElementException:  # 던파 메인 홈페이지로 시작할시
+    gameStartSp = driver.find_element_by_id('tmGameStart')
+    gameStartSp.click()
+    driver.close()
+    print("프로그램 실행 완료")
+    print("프로그램을 종료합니다...")
+    driver.quit()
+
+
